75e7e7b799773cee056ab3a7de6c3216cf4306fc
SGrondin <github@simongrondin.name>
2016-04-17 23:28:06 -0400

    2.0.0 del(), Class, mocha

--- src/Hotcache.coffee
+++ src/Hotcache.coffee
@@ -1,5 +1,4 @@
-# https://github.com/Mashape/HARchiver/blob/01b590cee12d50ed2349e426f291d73b4ee11698/src/cache.ml
-HPromise = try require "bluebird" catch e then Promise ? ->
+defaultPromise = try require "bluebird" catch e then Promise ? ->
 		throw new Error "Bottleneck: install 'bluebird' or use Node 0.12 or higher for Promise support"
 
 makeExpire = (cache, key, exp) ->
@@ -14,44 +13,46 @@ makeItem = (cache, key, exp, element=null) ->
 		waiting: []
 	}
 
-t = (promise) ->
-	if promise? then HPromise = promise
-	cache = {}
-	@get = (key, exp, fn, args..., cb) ->
-		if cache[key]?
-			if cache[key].element?
+class Hotcache
+	constructor: (@HPromise=defaultPromise) ->
+		@cache = {}
+	# https://github.com/Mashape/HARchiver/blob/01b590cee12d50ed2349e426f291d73b4ee11698/src/cache.ml
+	get: (key, exp, fn, args..., cb) =>
+		if @cache[key]?
+			if @cache[key].element?
 				# Found
-				cb.apply {}, cache[key].element
+				cb.apply {}, @cache[key].element
 			else
 				# Currently being fetched
-				cache[key].waiting.push cb
+				@cache[key].waiting.push cb
 		else
 			# Go get it yourself
-			newCached = makeItem cache, key, exp
-			cache[key] = newCached
-			cache[key].waiting.push cb
-			setTimeout () ->
-				fn.apply {}, Array::concat.call args, (res...) ->
+			newCached = makeItem @cache, key, exp
+			@cache[key] = newCached
+			@cache[key].waiting.push cb
+			setTimeout () =>
+				fn.apply {}, Array::concat.call args, (res...) =>
 					clearTimeout newCached.expire
 					if not res[0]?
-						cache[key] = makeItem cache, key, exp, res
+						@cache[key] = makeItem @cache, key, exp, res
 					else
-						delete cache[key]
+						delete @cache[key]
 
 					newCached.waiting.forEach (v) -> v.apply {}, res
 			, 0
-	@pget = (key, exp, fn, args...) =>
-		wrapped = (cb) ->
+	pget: (key, exp, fn, args...) =>
+		wrapped = (cb) =>
 			processed = try
-				if typeof (ref = fn.apply {}, args).then == 'function' then ref else HPromise.resolve ref
+				if typeof (ref = fn.apply {}, args)?.then == 'function' then ref else @HPromise.resolve ref
 			catch err
-				HPromise.reject err
+				@HPromise.reject err
 			processed
 			.then (args...) -> cb.apply {}, Array::concat null, args
 			.catch (args...) -> cb.apply {}, Array::concat {}, args
-		new HPromise (resolve, reject) =>
+		new @HPromise (resolve, reject) =>
 			@get.apply {}, Array::concat key, exp, wrapped, (error, args...) ->
 				(if error? then reject else resolve).apply {}, args
+	del: (keys...) => keys.forEach (k) => delete @cache[k]
 	@
 
-module.exports = t
+module.exports = Hotcache
--- test/index.js
+++ test/index.js
@@ -1,118 +1,201 @@
 var assert = require("assert")
 var Cache = require("../lib/index.js")
-var cache = new Cache()
-var nbChecks = 0
-var oldAssert = assert
-assert = function() {
-	++nbChecks
-	oldAssert.apply({}, arguments)
-}
 
-// Should be expired
-setTimeout(function(){
-	cache.get("abc", 1000, (function(a, b, cb){
-		assert(a === "z")
-		assert(b === "y")
-		cb(null, "woot", 123)
-	}), "z", "y", function(err, res1, res2){
-		assert(err === null)
-		assert(res1 === "woot")
-		assert(res2 === 123)
+describe("Hotcache", function(){
+	describe("get()", function(){
+		it("Should pass", function(done){
+			var cache = new Cache()
+			cache.get("abc", 1000, (function(a, cb){
+				assert(a === "z")
+				cb(null, "woot")
+			}), "z", function(err, res){
+				assert(err === null)
+				assert(res === "woot")
+				done()
+			})
+		})
+
+		it("Should also pass", function(done){
+			var cache = new Cache()
+			cache.get("abc", 1000, (function(a, cb){
+				assert(a === "z")
+				cb(null, "woot")
+			}), "z", function(err, res){
+				assert(err === null)
+				assert(res === "woot")
+			})
+
+			cache.get("abc", 1000, (function(a, cb){
+				assert(false)
+			}), "z", function(err, res){
+				assert(err === null)
+				assert(res === "woot")
+				done()
+			})
+		})
+
+		it("Should be expired", function(done){
+			this.timeout(3000)
+			var cache = new Cache()
+			cache.get("abc", 1000, (function(a, cb){
+				assert(a === "z")
+				cb(null, "woot")
+			}), "z", function(err, res){
+				assert(err === null)
+				assert(res === "woot")
+			})
+			setTimeout(function(){
+				cache.get("abc", 1000, (function(a, b, cb){
+					assert(a === "z")
+					assert(b === "y")
+					cb(null, "woot", 123)
+				}), "z", "y", function(err, res1, res2){
+					assert(err === null)
+					assert(res1 === "woot")
+					assert(res2 === 123)
+					done()
+				})
+			}, 2000)
+		})
+
+		it("Should still be in cache", function(done){
+			var cache = new Cache()
+			cache.get("abc", 1000, (function(a, cb){
+				assert(a === "z")
+				cb(null, "woot")
+			}), "z", function(err, res){
+				assert(err === null)
+				assert(res === "woot")
+			})
+
+			setTimeout(function(){
+				cache.get("abc", 1000, (function(a, cb){
+					assert(false)
+				}), "z", function(err, res){
+					assert(err === null)
+					assert(res === "woot")
+					done()
+				})
+			}, 500)
+		})
+
+		it("Should fail", function(done){
+			var cache = new Cache()
+			cache.get("abc", 1000, (function(a, cb){
+				assert(a === "z")
+				cb("woops", "woot")
+			}), "z", function(err, res){
+				assert(err === "woops")
+				assert(res === "woot")
+				done()
+			})
+		})
+
 	})
-}, 2000)
 
-// Should still be in cache
-setTimeout(function(){
-	cache.get("abc", 1000, (function(a, cb){
-		assert(false)
-		cb(null, "woot")
-	}), "z", function(err, res){
-		assert(err === null)
-		assert(res === "woot")
+	describe("pget()", function(){
+		it("Should accept naked value", function(){
+			var cache = new Cache()
+			return cache.pget("def", 1000, (function(a){
+				assert(a === "z")
+				return 6
+			}), "z")
+			.then(function(res){
+				assert(res === 6)
+			})
+		})
+
+		it("Should accept wrapped value", function(){
+			var cache = new Cache()
+			return cache.pget("ghi", 1000, (function(a){
+				assert(a === "z")
+				return Promise.resolve(6)
+			}), "z")
+			.then(function(res){
+				assert(res === 6)
+			})
+		})
+
+
+		it("Should not fetch twice", function(){
+			var cache = new Cache()
+			cache.pget("ghi", 1000, (function(a){
+				return 111
+			}))
+
+			return cache.pget("ghi", 1000, (function(a){
+				throw new Error("Shouldn't be called")
+			}))
+		})
+
+		it("Should catch the error", function(){
+			var cache = new Cache()
+			return cache.pget("jkl", 1000, (function(a){
+				nothing.nothing() // Throws an error
+				return Promise.resolve(6)
+			}), "z")
+			.then(function(res){
+				return Promise.reject(Error("Shouldn't continue the chain"))
+			})
+			.catch(function(err){
+				assert(err.message === "nothing is not defined")
+				return Promise.resolve()
+			})
+		})
+
+		it("Should accept naked null", function(){
+			var cache = new Cache()
+			return cache.pget("def", 1000, (function(a){
+				assert(a === "z")
+				return null
+			}), "z")
+			.then(function(res){
+				assert(res === null)
+			})
+		})
+
+		it("Should accept wrapped null", function(){
+			var cache = new Cache()
+			return cache.pget("ghi", 1000, (function(a){
+				assert(a === "z")
+				return Promise.resolve(null)
+			}), "z")
+			.then(function(res){
+				assert(res === null)
+			})
+		})
 	})
-}, 500)
 
-// These 2 should fail
-cache.get("abc", 1000, (function(a, cb){
-	assert(a === "z")
-	cb("woops", "woot")
-}), "z", function(err, res){
-	assert(err === "woops")
-	assert(res === "woot")
-})
+	describe("del()", function(){
+		it("Should delete keys", function(){
+			var cache = new Cache()
 
-cache.get("abc", 1000, (function(a, cb){
-	assert(false)
-	cb(null, "woot")
-}), "z", function(err, res){
-	assert(err === "woops")
-	assert(res === "woot")
-})
+			cache.pget("abc", 1000, (function(a){
+				assert(a === "z")
+				return 4
+			}), "z")
+			.then(function(res){
+				assert(res === 6)
+			})
 
-// These 2 should pass
-setTimeout(function() {
-	cache.get("abc", 1000, (function(a, cb){
-		assert(a === "z")
-		cb(null, "woot")
-	}), "z", function(err, res){
-		assert(err === null)
-		assert(res === "woot")
+			cache.pget("def", 1000, (function(a){
+				assert(a === "z")
+				return 6
+			}), "z")
+			.then(function(res){
+				assert(res === 4)
+			})
+
+			var dump = Object.keys(cache.cache).sort()
+			assert(dump.length === 2)
+			assert(dump[0] === "abc")
+			assert(dump[1] === "def")
+
+			cache.del("abc", "def")
+
+			var dump2 = Object.keys(cache.cache).sort()
+			assert(dump2.length === 0)
+		})
 	})
 
-	cache.get("abc", 1000, (function(a, cb){
-		assert(false)
-		cb(null, "woot")
-	}), "z", function(err, res){
-		assert(err === null)
-		assert(res === "woot")
-	})
-}, 250)
-
-/****** PROMISES *****/
-// No-wrap
-cache.pget("def", 1000, (function(a){
-	assert(a === "z")
-	return 6
-}), "z")
-.then(function(res){
-	assert(res === 6)
 })
-.catch(function(err){
-	throw err
-})
-
-// Wrap
-cache.pget("ghi", 1000, (function(a){
-	assert(a === "z")
-	return Promise.resolve(6)
-}), "z")
-.then(function(res){
-	assert(res === 6)
-})
-.catch(function(err){
-	throw err
-})
-
-// Shouldn't get called
-cache.pget("ghi", 1000, (function(a){
-	throw new Error("Shouldn't be called")
-}))
-
-// Should catch the error
-cache.pget("jkl", 1000, (function(a){
-	nothing.nothing() // Throws an error
-	return Promise.resolve(6)
-}), "z")
-.then(function(res){
-	throw new Error("Shouldn't continue the chain")
-})
-.catch(function(err){
-	assert(err.message === "nothing is not defined")
-})
-
-
-
-setTimeout(function() {
-	oldAssert(nbChecks === 22)
-	console.log("Success", nbChecks, "/22 tests")
-}, 2500)
